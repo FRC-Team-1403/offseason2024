3ea9e866aeecdfc82aeba72854ba2f96a8bc8f5a
anishka-k <96264434+anishka-k@users.noreply.github.com>
2023-12-05 15:24:04 -0500

    remove cougarlib submodule

--- src/main/java/team1403/robot/__replaceme__/sensorBoard/SensorBoard.java
+++ src/main/java/team1403/robot/__replaceme__/sensorBoard/SensorBoard.java
@@ -1,4 +1,6 @@
 package team1403.robot.__replaceme__.sensorBoard;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import team1403.lib.core.CougarLibInjectedParameters;
 import team1403.lib.core.CougarSubsystem;
@@ -6,14 +8,16 @@ import team1403.lib.device.wpi.WpiLimitSwitch;
 
 public class SensorBoard extends CougarSubsystem{
     private final WpiLimitSwitch limitSwitch;
+    private final Encoder encoder;
     
     public SensorBoard(String name, CougarLibInjectedParameters injectedParameters) {
         super(name, injectedParameters);
         limitSwitch = new WpiLimitSwitch(name, 0);
+        encoder = new Encoder(0, 1);
       }
-
     public void periodic() {
         SmartDashboard.putBoolean("ma3", limitSwitch.get());
+        SmartDashboard.putData(encoder);
     }
     
 }
